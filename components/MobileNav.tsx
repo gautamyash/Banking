eab33b6aad0bad87c9569e6deac2d07787f93a4b
gautamyash <gautamyash37@gmail.com>
2024-08-09 21:17:21 +0530

    Implementation of sign in for with the help of Zod

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -30,7 +30,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
         <SheetContent side="left" className="border-none bg-white ">
           <Link
             href="/"
-            className="mb-12 cursor-pointer flex items-center gap-1"
+            className="cursor-pointer flex items-center gap-1 px-4"
           >
             <Image src="/icons/logo.svg" width={34} height={34} alt="Logo" />
             <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
